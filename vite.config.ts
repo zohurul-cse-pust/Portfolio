2a2735a67f2717a8699328446f6a42f3a05636bb
Zohurul Islam <mdzohu@gmail.com>
2026-08-05 12:12:58 +0600

    Configure base path and GitHub Actions workflow for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
